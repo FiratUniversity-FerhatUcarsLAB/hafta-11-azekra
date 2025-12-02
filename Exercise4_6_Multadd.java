4f0a7105483011907f860f4f9fe024eace906c7d
Anas Zekra <anaszekra19@gmail.com>
2025-12-02 19:57:45 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,39 @@
+/*
+ * Ad Soyad: [ENES ZUKRA]
+ * Ogrenci No: [250541616]
+ * Tarih: [02.12.2025] 
+ * Aciklama: Exercise 4.6
+ */
+
 public class Exercise4_6_Multadd {
 
+    // Method to perform a * b + c
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        return a * b + c;
     }
 
+    // Method to calculate: x*e^(-x) + sqrt(1 - e^(-x))
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+        return multadd(x, Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        // Test multadd with simple example
+        double result1 = multadd(2, 3, 4); // 2*3 + 4 = 10
+        System.out.println("multadd(2, 3, 4) = " + result1);
+
+        // Using multadd to compute sin(pi/4) + cos(pi/4)/2
+        double piOver4 = Math.PI / 4;
+        double trigResult = multadd(Math.cos(piOver4), 0.5, Math.sin(piOver4));
+        System.out.println("sin(pi/4) + cos(pi/4)/2 = " + trigResult);
+
+        // Using multadd to compute log10 + log20
+        double logResult = multadd(Math.log(10), 1, Math.log(20));
+        System.out.println("log10 + log20 = " + logResult);
+
+        // Test expSum method
+        double x = 2.0;
+        double expSumResult = expSum(x);
+        System.out.println("expSum(" + x + ") = " + expSumResult);
     }
 }
